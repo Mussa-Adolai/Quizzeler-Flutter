ddf1424410b2e0556c879bbf0cdc304800973dc4
Mussa-Adolai <60032727+Mussa-Adolai@users.noreply.github.com>
2022-08-10 21:07:57 +0300

    Abstract 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'dart:ffi';
-import 'question.dart';
 
 import 'package:flutter/material.dart';
+import 'package:quzzler_test/Quiz_brain.dart';
+import 'Quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() {
   runApp(
@@ -44,13 +47,7 @@ class _QuizPageState extends State<QuizPage> {
   //   'A slug\'s blood is green.', //t
   // ];
   // List<bool> answer = [false, true, true];
-  List<Question> questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs .', a: false),
-    Question(
-        q: 'Approximately one quarter of human bones are in the feet .',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true),
-  ];
+
   int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
@@ -65,7 +62,7 @@ class _QuizPageState extends State<QuizPage> {
               padding: EdgeInsets.symmetric(horizontal: 10.0),
               child: Text(
                 // 'This is where the question text will go ',
-                questionBank[questionNumber].questionText,
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -86,7 +83,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 bool correctAnswer =
-                    questionBank[questionNumber].questionAnswer;
+                    quizBrain.questionBank[questionNumber].questionAnswer;
                 if (correctAnswer == true) {
                   scoreKeeper.add(
                     Icon(
@@ -126,7 +123,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 bool correctAnswer =
-                    questionBank[questionNumber].questionAnswer;
+                    quizBrain.questionBank[questionNumber].questionAnswer;
                 if (correctAnswer == false) {
                   scoreKeeper.add(
                     Icon(
